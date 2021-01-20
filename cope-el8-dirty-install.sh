611d0ed9adec7190be2f82d91b192b48120ec8d6
Josef Vybíhal <josef.vybihal@gmail.com>
2021-01-20 17:13:01 +0100

    lc

--- cope-el8-dirty-install.sh
+++ cope-el8-dirty-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dnf install epel-release -y
-yum config-manager --set-enabled PowerTools && dnf update -y
+yum config-manager --set-enabled powertools && dnf update -y
 rpm -Uvh https://github.com/xvybihal/cope/releases/download/v1.1-rc2/perl-Env-Path-0.19-1.el8.noarch.rpm
 rpm -Uvh https://github.com/xvybihal/cope/releases/download/v1.1-rc2/perl-IO-Stty-0.03-10.el8.noarch.rpm
 rpm -Uvh https://github.com/xvybihal/cope/releases/download/v1.1-rc2/perl-Number-Format-1.73-14.el8.noarch.rpm
